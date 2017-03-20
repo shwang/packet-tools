67282836b9fee59d912d84b3850d8ddae3240540
Steven H. Wang <wang.steven.h@gmail.com>
2017-03-20 14:54:50 -0700

    build VLAN and bare NSH packets

--- trial.go
+++ trial.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"time"
 
 	"github.com/google/gopacket"
@@ -29,7 +30,7 @@ func main() {
 
 	pktSrc := gopacket.NewPacketSource(inHandle, inHandle.LinkType())
 	for pkt := range pktSrc.Packets() {
-		modPkt, err := modifyPacket(pkt)
+		modPkt, err := modifyPacketBareNSH(pkt)
 		fmt.Println("original")
 		fmt.Println(pkt)
 		fmt.Println()
@@ -78,32 +79,140 @@ func modifyPacket(pkt gopacket.Packet) (gopacket.Packet, error) {
 	ipExpectGRE := *(pkt.NetworkLayer().(*layers.IPv4)) // Create a copy of the original IP layer
 	ipExpectGRE.Protocol = layers.IPProtocolGRE
 
-	gre := &layers.GRE{Protocol: nsh.EthernetTypeNSH}
-	nshLayer := nsh.NSH{
-		Version: 0,
-		Length: 6,
-		Protocol: nsh.NSHProtocolIPv4,
-		MDType: nsh.MDTypeOne,
+	ipModifiedDest := *(pkt.NetworkLayer().(*layers.IPv4))
+	ipModifiedDest.DstIP = net.IPv4(10, 13, 13, 13)
 
-		ServicePathIdentifier: 777,
-		ServiceIndex: 7,
-		Context: [4]nsh.NSHContextHeader{1,2,3,4},
-	}
+	gre := &layers.GRE{Protocol: layers.EthernetTypeIPv4}
+	// gre := &layers.GRE{Protocol: nsh.EthernetTypeNSH}
+	// nshLayer := nsh.NSH{
+	// 	Version: 0,
+	// 	Length: 6,
+	// 	Protocol: nsh.NSHProtocolIPv4,
+	// 	MDType: nsh.MDTypeOne,
+
+	// 	ServicePathIdentifier: 777,
+	// 	ServiceIndex: 7,
+	// 	Context: [4]nsh.NSHContextHeader{1,2,3,4},
+	// }
 
 	decodableNetwork, ok := pkt.NetworkLayer().(gopacket.DecodingLayer)
 	fmt.Println("gre next layer", gre.NextLayerType(), "\n", "network next layer", decodableNetwork.NextLayerType())
 
 	buf := gopacket.NewSerializeBuffer()
-	gopacket.SerializeLayers(buf, gopacket.SerializeOptions{FixLengths: true, ComputeChecksums:true},
+	gopacket.SerializeLayers(buf, gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true},
+		transport,
+		application)
+
+	fullOpts := gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true}
+	ipModifiedDest.SerializeTo(buf, fullOpts)
+	//nshLayer.SerializeTo(buf, fullOpts)
+	gre.SerializeTo(buf, fullOpts)
+	ipExpectGRE.SerializeTo(buf, fullOpts)
+	link.SerializeTo(buf, fullOpts)
+
+	return gopacket.NewPacket(buf.Bytes(),
+		layers.LayerTypeEthernet, gopacket.Default), nil
+}
+
+func modifyPacketBareNSH(pkt gopacket.Packet) (gopacket.Packet, error) {
+	link, ok := pkt.LinkLayer().(gopacket.SerializableLayer)
+	if !ok {
+		return nil, fmt.Errorf("Link layer is not serialiable %q\n", link)
+	}
+
+	network, ok := pkt.NetworkLayer().(gopacket.SerializableLayer)
+	if !ok {
+		return nil, fmt.Errorf("Network layer is not serializable %q\n", network)
+	}
+
+	transport, ok := pkt.TransportLayer().(gopacket.SerializableLayer)
+	if !ok {
+		return nil, fmt.Errorf(
+			"Transport layer is not serializable %q\n", transport)
+	}
+
+	application, ok := pkt.ApplicationLayer().(gopacket.SerializableLayer)
+	if !ok {
+		return nil, fmt.Errorf(
+			"Application layer is not serializable %q\n", application)
+	}
+
+	etherExpectNSH := *(link.(*layers.Ethernet)) // Create a copy of the original IP layer
+	etherExpectNSH.EthernetType = nsh.EthernetTypeNSH
+
+	nshLayer := nsh.NSH{
+		Version:  0,
+		Length:   6,
+		Protocol: nsh.NSHProtocolIPv4,
+		MDType:   nsh.MDTypeOne,
+
+		ServicePathIdentifier: 777,
+		ServiceIndex:          7,
+		Context:               [4]nsh.NSHContextHeader{1, 2, 3, 4},
+	}
+
+	fmt.Println("link next layer", etherExpectNSH.NextLayerType())
+
+	buf := gopacket.NewSerializeBuffer()
+	gopacket.SerializeLayers(buf, gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true},
 		transport,
 		application)
 
 	fullOpts := gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true}
 	network.SerializeTo(buf, fullOpts)
 	nshLayer.SerializeTo(buf, fullOpts)
-	gre.SerializeTo(buf, fullOpts)
-	ipExpectGRE.SerializeTo(buf, fullOpts)
-	link.SerializeTo(buf, fullOpts)
+	etherExpectNSH.SerializeTo(buf, fullOpts)
+
+	return gopacket.NewPacket(buf.Bytes(),
+		layers.LayerTypeEthernet, gopacket.Default), nil
+
+}
+
+// Create a VLAN variant of this packet
+func modifyPacketVLAN(pkt gopacket.Packet) (gopacket.Packet, error) {
+	link, ok := pkt.LinkLayer().(gopacket.SerializableLayer)
+	if !ok {
+		return nil, fmt.Errorf("Link layer is not serialiable %q\n", link)
+	}
+
+	network, ok := pkt.NetworkLayer().(gopacket.SerializableLayer)
+	if !ok {
+		return nil, fmt.Errorf("Network layer is not serializable %q\n", network)
+	}
+
+	transport, ok := pkt.TransportLayer().(gopacket.SerializableLayer)
+	if !ok {
+		return nil, fmt.Errorf(
+			"Transport layer is not serializable %q\n", transport)
+	}
+
+	application, ok := pkt.ApplicationLayer().(gopacket.SerializableLayer)
+	if !ok {
+		return nil, fmt.Errorf(
+			"Application layer is not serializable %q\n", application)
+	}
+	linkExpectVLAN := *(link.(*layers.Ethernet))
+	linkExpectVLAN.EthernetType = layers.EthernetTypeDot1Q
+
+	vlan := layers.Dot1Q{
+		Priority:       10,
+		VLANIdentifier: 13,
+		Type:           layers.EthernetTypeIPv4,
+	}
+
+	//ipModifiedDest := *(pkt.NetworkLayer().(*layers.IPv4))
+	//ipModifiedDest.DstIP = net.IPv4(10, 13, 13, 13)
+
+	buf := gopacket.NewSerializeBuffer()
+	gopacket.SerializeLayers(buf, gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true},
+		transport,
+		application)
+
+	fullOpts := gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true}
+	//ipModifiedDest.SerializeTo(buf, fullOpts)
+	network.SerializeTo(buf, fullOpts)
+	vlan.SerializeTo(buf, fullOpts)
+	linkExpectVLAN.SerializeTo(buf, fullOpts)
 
 	return gopacket.NewPacket(buf.Bytes(),
 		layers.LayerTypeEthernet, gopacket.Default), nil
